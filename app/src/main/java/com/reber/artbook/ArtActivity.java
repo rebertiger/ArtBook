6df9958b86d015d68ae32fa424efdfc7a4a982fc
Recep Berke KAPLAN <recepberkekaplan@gmail.com>
2023-02-24 16:59:44 +0300

    Add delete function.

--- app/src/main/java/com/reber/artbook/ArtActivity.java
+++ app/src/main/java/com/reber/artbook/ArtActivity.java
@@ -35,6 +35,7 @@ public class ArtActivity extends AppCompatActivity {
     ActivityResultLauncher<String> permissionLauncher;
     Bitmap selectedImage;
     SQLiteDatabase database;
+    int artId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,95 +44,99 @@ public class ArtActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         registerLauncher(); //register
-        database = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
-
+        database = this.openOrCreateDatabase("Arts", MODE_PRIVATE, null);
 
 
         Intent intent = getIntent();
         String info = intent.getStringExtra("info");
-        if (info.equals("new")){
+        if (info.equals("new")) {
+            binding.deleteButton.setVisibility(View.GONE);
             //add new data
             binding.nameText.setText("");
             binding.artistText.setText("");
             binding.yearText.setText("");
             binding.saveButton.setVisibility(View.VISIBLE);
             binding.imageView.setImageResource(R.drawable.selectimage);
-        }else{
-            int artId = intent.getIntExtra("artId",0);
+        } else {
+            binding.deleteButton.setVisibility(View.VISIBLE);
+            artId = intent.getIntExtra("artId", 0);
             binding.saveButton.setVisibility(View.INVISIBLE);
 
-            try{
-                Cursor cursor = database.rawQuery("SELECT * FROM arts WHERE id = ?",new String[]{String.valueOf(artId)});
+            try {
+                Cursor cursor = database.rawQuery("SELECT * FROM arts WHERE id = ?", new String[]{String.valueOf(artId)});
                 int artNameIx = cursor.getColumnIndex("artname");
                 int painterNameIx = cursor.getColumnIndex("paintername");
                 int yearIx = cursor.getColumnIndex("year");
                 int imageIx = cursor.getColumnIndex("image");
 
 
-                while (cursor.moveToNext()){
+                while (cursor.moveToNext()) {
                     //set info's
                     binding.nameText.setText(cursor.getString(artNameIx));
                     binding.artistText.setText(cursor.getString(painterNameIx));
                     binding.yearText.setText(cursor.getString(yearIx));
                     //bitmap needded to set image
                     byte[] bytes = cursor.getBlob(imageIx);
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                     binding.imageView.setImageBitmap(bitmap);
                 }
                 cursor.close();
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-    public void save(View view){
+
+    public void save(View view) {
         //save
         String name = binding.nameText.getText().toString();
         String artistname = binding.artistText.getText().toString();
         String year = binding.yearText.getText().toString();
         //image to byte
-        Bitmap smallImage = makeSmallerImage(selectedImage,300);
+        Bitmap smallImage = makeSmallerImage(selectedImage, 300);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        smallImage.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        smallImage.compress(Bitmap.CompressFormat.PNG, 50, outputStream);
         byte[] byteArray = outputStream.toByteArray();
 
 
-        try{
+        try {
             database.execSQL("CREATE TABLE IF NOT EXISTS arts(id INTEGER PRIMARY KEY, artname VARCHAR, paintername VARCHAR, year VARCHAR, image BLOB)");
             String sqlString = "INSERT INTO arts (artname,paintername,year,image) VALUES(?, ?, ?, ?)";
             SQLiteStatement sqLiteStatement = database.compileStatement(sqlString);
-            sqLiteStatement.bindString(1,name);
-            sqLiteStatement.bindString(2,artistname);
-            sqLiteStatement.bindString(3,year);
-            sqLiteStatement.bindBlob(4,byteArray);
+            sqLiteStatement.bindString(1, name);
+            sqLiteStatement.bindString(2, artistname);
+            sqLiteStatement.bindString(3, year);
+            sqLiteStatement.bindBlob(4, byteArray);
             sqLiteStatement.execute();
 
-        }catch(Exception e){
+        } catch (Exception e) {
             //except
             e.printStackTrace();
         }
-        Intent intent = new Intent(ArtActivity.this,MainActivity.class);
+        Intent intent = new Intent(ArtActivity.this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
-    public Bitmap makeSmallerImage(Bitmap image, int maxSize){
+
+    public Bitmap makeSmallerImage(Bitmap image, int maxSize) {
         int width = image.getWidth();
         int height = image.getHeight();
-        float bitmapRatio = (float)width/(float)height;
-        if (bitmapRatio >1){
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
             //landscape
             width = maxSize;
-            height = (int)(width/bitmapRatio);
-        }else{
+            height = (int) (width / bitmapRatio);
+        } else {
             //portrait
             height = maxSize;
-            width = (int) (height*bitmapRatio);
+            width = (int) (height * bitmapRatio);
         }
-        Bitmap scaledBitmap = Bitmap.createScaledBitmap(image,width,height,true);
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(image, width, height, true);
         return scaledBitmap;
     }
-    public void selectImage(View view){
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+
+    public void selectImage(View view) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             //Android 13 Tiramisu read media images perm
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_MEDIA_IMAGES)) {
@@ -142,16 +147,16 @@ public class ArtActivity extends AppCompatActivity {
                             permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
                         }
                     }).show();
-                } else{
+                } else {
                     //req permisson
                     permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
                 }
-            } else{
+            } else {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 activityResultLauncher.launch(galleryIntent);
 
             }
-        } else{
+        } else {
             //Lower than API33
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
@@ -162,11 +167,11 @@ public class ArtActivity extends AppCompatActivity {
                             permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
                         }
                     }).show();
-                } else{
+                } else {
                     //req permisson
                     permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
                 }
-            } else{
+            } else {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 activityResultLauncher.launch(galleryIntent);
 
@@ -174,28 +179,29 @@ public class ArtActivity extends AppCompatActivity {
         }
 
     }
+
     private void registerLauncher() {
         activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
             @Override
             public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == RESULT_OK){
+                if (result.getResultCode() == RESULT_OK) {
                     Intent intentFromResult = result.getData();
-                    if (intentFromResult != null){
+                    if (intentFromResult != null) {
                         //get location of image
                         Uri imageData = intentFromResult.getData();
                         //binding.imageView.setImageURI(imageData); // if you want to just set, but not to save to database
-                        try{
-                            if(Build.VERSION.SDK_INT>=28){
-                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(),imageData);
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), imageData);
                                 selectedImage = ImageDecoder.decodeBitmap(source);
                                 binding.imageView.setImageBitmap(selectedImage);
                             } else {
-                                selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(),imageData);
+                                selectedImage = MediaStore.Images.Media.getBitmap(ArtActivity.this.getContentResolver(), imageData);
                                 binding.imageView.setImageBitmap(selectedImage);
                             }
 
 
-                        } catch(Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
@@ -205,16 +211,37 @@ public class ArtActivity extends AppCompatActivity {
         permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
             @Override
             public void onActivityResult(Boolean result) {
-                if (result){
+                if (result) {
                     //permission granted
                     //go to gallery
 
-                } else{
+                } else {
                     //denied
                     Toast.makeText(ArtActivity.this, "Permission needed.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        
+
+    }
+
+    public void delete(View view) {
+        //delete function
+        try {
+            Cursor cursor = database.rawQuery("SELECT * FROM arts",null);
+            database.execSQL("DELETE FROM arts WHERE id = ?", new String[]{String.valueOf(artId)});
+
+            //set info's
+            binding.nameText.setText("");
+            binding.artistText.setText("");
+            binding.yearText.setText("");
+            binding.imageView.setImageResource(R.drawable.selectimage);
+            cursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(ArtActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
     }
 }
